96b9c822bfaedf56f16ca4b229f1f60b902ae564
c0ntr0ller <bek.andrey@gmail.com>
2015-09-24 19:42:01 +0600

    args

---
+++ CP/test/FirstTest.java
@@ -0,0 +1,25 @@
+import junit.framework.TestCase;
+import sun.applet.Main;
+
+/**
+ * Created by Bek on 24.09.2015.
+ */
+public class FirstTest extends TestCase {
+    public void setUp() throws Exception{
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception{
+        super.tearDown();
+    }
+    public void testMaxInt()throws Exception{
+        int result = myMain.maxInt(1,3);
+        assertEquals(3, result);
+    }
+
+    public void testAvgDouble() throws Exception{
+        double dresult = (new myMain()).avgDouble(15, 45);
+        assertEquals(30., dresult);
+    }
+
+}
